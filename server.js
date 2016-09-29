576bc03bc2a0bb460265c76b95af7754fd544f72
Rajeswari0906 <rajes.jayaraman@gmail.com>
2016-09-29 20:20:13 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname,  'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
+app.get('/article-one', function (req, res) {
   res.send('Article one requested and will be served here');
 });
 
-app.get('/article-two',function (req, res){
+app.get('/article-two', function (req, res) {
   res.send('Article two requested and will be served here');
 });
 
-app.get('/article-three',function(req,res){
+app.get('/article-three', function (req, res) {
     res.send('Article three requested and will be served here');
 });
 
-app.get('/ui/style.css',function(req,res){
-    res.sendfile(path.join(__dirname,'ui', 'style.css'));
+app.get('/ui/style.css', function (req, res) {
+    res.sendfile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png',funtion(req,res){
-    res.sendfile(path.join(__dirname,'ui', 'madi.png'));
+app.get('/ui/madi.png', funtion (req, res) {
+    res.sendfile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
